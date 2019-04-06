40d83e4721259a992ba6f28901d0a390ad5debf0
piokrako <piokrako@gmail.com>
2019-04-06 10:30:50 +0200

    Created basic structure of combobox

---
+++ combobox.js
@@ -0,0 +1,6 @@
+var combo = document.getElementById("combobox");
+combo.addEventListener("click", toggleCombo, false);
+function toggleCombo() {
+  var comboList = document.getElementById("combobox-list");
+  comboList.classList.toggle("hide");
+}
