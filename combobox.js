64176ecd2eb0bc1b08a2881ad43c585a9061be38
piokrako <piokrako@gmail.com>
2019-04-06 10:49:11 +0200

    Added internations

--- combobox.js
+++ combobox.js
@@ -1,6 +1,12 @@
-var combo = document.getElementById("combobox");
+const combo = document.getElementById("combobox");
 combo.addEventListener("click", toggleCombo, false);
 function toggleCombo() {
   var comboList = document.getElementById("combobox-list");
   comboList.classList.toggle("hide");
 }
+
+
+function selectComboList(elementText) {
+  combo.innerText = elementText;
+    toggleCombo(); 
+  }
